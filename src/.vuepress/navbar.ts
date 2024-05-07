93b14d0e7b8a12213555bae3180c4cc6500d8dc6
JesusAlbujas <jesusramirez35000@gmail.com>
2024-05-07 13:38:48 -0400

    feat: Add `devices` section in download navbar

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -162,13 +162,14 @@ export const enNavbarConfig = navbar([
       {
         text: "Actualizaciones",
         children: [
-          "updates/", 
+          "updates/",
+          "updates/adempiere-3.9.4",
           "updates/rs-5.x", 
           "updates/rs-4.x",
           "updates/rs-3.x",
           "updates/rs-2.x",
           "updates/rs-1.x",
-          "updates/adempiere-3.9.4"
+          "updates/devices"
         ],
       },
     ],
